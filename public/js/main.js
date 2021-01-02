4dcd5060fcd17645d076a82e7ea1ce7431fb9bb1
zulma <zumendez7@gmail.com>
2021-01-02 16:54:53 +0100

    exo fini

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+//  # Exercice 3 JavaScript -> Array
+
+//     - ## Créer une variable qui reçois un array avec 3 éléments -> ex: pomme, cerise, fraise
+let fruits = ["pomme", "cerise", "fraise"]
+
+//     - ## Afficher les éléments de l'array
+console.log(fruits)
+
+//     - ## Ajouter votre prenom à l'array et afficher son contenu
+fruits.push('Zulma')
+console.log(fruits)
+
+//     - ## Remplacer et afficher un élément de l'array par autre chose
+fruits.splice(0, 1, 'banane')
+console.log(fruits)
